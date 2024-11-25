6343ae918d0cf46bee4a6c9c2c819211658c7a88
anonuser07 <82811255+anonuser07@users.noreply.github.com>
2024-11-25 23:35:44 +0530

    Add files via upload

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-rest-params */
+/* eslint-disable @typescript-eslint/no-this-alias */
+import clsx, { ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
+/** Merge classes with tailwind-merge with clsx full feature */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function throttle(callback: Function, delay: number) {
+  let wait = false;
+  return function () {
+    if (!wait) {
+      callback();
+      wait = true;
+      setTimeout(function () {
+        wait = false;
+      }, delay);
+    }
+  };
+}
