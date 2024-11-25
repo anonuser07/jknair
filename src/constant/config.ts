440be844344d2b4867885d8b51be48aac6d774df
anonuser07 <82811255+anonuser07@users.noreply.github.com>
2024-11-25 23:33:05 +0530

    Add files via upload

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,1 +1,72 @@
+export const siteConfig = {
+  title: 'J K Nair | Vital Training | Recognised Corporate Trainer in India',
+  description:
+    '18 Years of experience in HR and Training. Trained more than 2 lakhs of employees in various corporate. More than 120 corporate clients',
+  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
+  url: 'https://www.jknairvitaltraining.com',
+};
 
+export const booksList = [
+  {
+    name: 'Success Fundas',
+    anchor: 'success-fundas',
+    image: '/assets/books/success-fundas.jpg',
+    description: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt',
+    buyLink: 'https://www.amazon.in/Success-Fundas-J-K-Nair/dp/9391085083'
+  },
+  {
+    name: 'Developing Leadership Muscles',
+    anchor: 'developing-leadership-muscles',
+    image: '/assets/books/developing-leadership-muscles.jpg',
+    description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt',
+    buyLink: 'https://www.amazon.in/DEVELOPING-LEADERSHIP-MUSCLES-J-Nair/dp/939108513X'
+  }
+]
+
+export const serviceList = [
+  {
+    name: 'Improving Work Culture',
+    logo: '',
+    image: '/assets/modules/work.png',
+    brief:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    details: '',
+    anchor: 'work',
+  },
+  {
+    name: 'Stress Management',
+    logo: '',
+    image: '/assets/modules/stress.png',
+    brief:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+    details: '',
+    anchor: 'stress',
+  },
+  {
+    name: 'Presentation Skills',
+    logo: '',
+    image: '/assets/modules/presentation.png',
+    brief:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+    details: '',
+    anchor: 'presentation',
+  },
+  {
+    name: 'Leadership Versatility',
+    logo: '',
+    image: '/assets/modules/leadership.png',
+    brief:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    details: '',
+    anchor: 'leadership',
+  },
+  {
+    name: 'Customised Module',
+    logo: '',
+    image: '/assets/modules/custom.jpg',
+    brief:
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam',
+    details: '',
+    anchor: 'custom',
+  },
+];
